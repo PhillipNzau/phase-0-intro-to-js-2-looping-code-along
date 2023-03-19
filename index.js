7ee0cc8422c74c23e09f0f755de6664c4a206526
phillip <nzauonline@gmail.com>
2023-03-19 16:03:41 +0300

    Assignment Complete

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+const messages = []
+function writeCards(nameArr, eventName) {
+    for(let i =0; i < nameArr.length; i++) {
+        messages.push(`Thank you, ${nameArr[i]}, for the wonderful ${eventName} gift!`)
+    }
+    return messages;
+}
+function countDown(number) {
+    while (number >=0) {
+        console.log(number);
+        number--;
+    }
+}
